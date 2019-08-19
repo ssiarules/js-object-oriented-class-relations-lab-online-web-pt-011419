dee66c9dfb63245ff4e5003d834ad409bab46baa
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-19 03:58:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+let store = {drivers:[], passengers:[], trips:[]}
+
+let driverId = 0
+let passengerId = 0
+let tripId = 0
+
 class Driver {
-  constructor(id, name){
-    this.id = id
+  constructor(name){
     this.name = name
+    this.id = ++driverId
+    store.drivers.push(this)
+  }
+  trip(){
+    return store.trips.filter(trip => trip.driverId === this.id)
   }
 }
