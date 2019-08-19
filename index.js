fe19aa0cf015578d7a6bbe121467a3065cad0990
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-19 04:10:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-let store = {drivers:[], passengers:[], trips:[]}
+let store = {drivers: [], passengers: [], trips: []}
 
 let driverId = 0
 let passengerId = 0
 let tripId = 0
 
 class Driver {
-  constructor(name){
-    this.name = name
-    this.id = ++driverId
-    store.drivers.push(this)
-  }
-  trip(){
-    return store.trips.filter(trip => trip.driverId === this.id)
-  }
-  passengers(){
-    return store.passengers.filter(passenger => passenger.driverId === this.Id)
-  }
+    constructor(name){
+        this.name = name
+        this.id = ++driverId
+        store.drivers.push(this)
+    }
+    trips(){
+        return store.trips.filter(trip => trip.driverId === this.id)
+    }
+
+    passengers(){
+        return store.passengers.filter(passenger => passenger.driverId === this.Id)
+    }
 }
+
 class Passenger{
     constructor(name){
         this.name = name
