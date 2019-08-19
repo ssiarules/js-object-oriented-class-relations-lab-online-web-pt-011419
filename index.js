a694ea4bbf582a8a5111e6bedb093a17dec4aa18
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-19 04:07:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,39 @@ class Driver {
     return store.passengers.filter(passenger => passenger.driverId === this.Id)
   }
 }
+class Passenger{
+    constructor(name){
+        this.name = name
+        this.id = ++passengerId
+        store.passengers.push(this)
+    }
+
+    trips(){
+        return store.trips.filter(trip => trip.passengerId === this.id)
+    }
+
+    drivers(){
+        return this.trips().map(trip => {return trip.driver()})
+
+    }
+}
+
+class Trip{
+    constructor(driver, passenger){
+        this.id = ++tripId
+        this.driverId = driver.id
+        this.passengerId = passenger.id
+        store.trips.push(this)
+    }
+
+    driver(){
+        return store.drivers.find(driver => {
+            return driver.id === this.driverId
+        })
+    }
+    passenger(){
+        return store.passengers.find(passenger => {
+            return passenger.id === this.passengerId
+        })
+    }
+}
